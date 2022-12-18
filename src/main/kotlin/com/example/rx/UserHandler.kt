c100fc3b5cac77fc9210244a2f385d69b95025b2
easywaldo <easywaldoji@gmail.com>
2022-12-19 00:09:37 +0900

    Nested router function - users handler added

---
+++ src/main/kotlin/com/example/rx/UserHandler.kt
@@ -0,0 +1,27 @@
+package com.example.rx
+
+import org.springframework.stereotype.Component
+import org.springframework.web.reactive.function.server.ServerRequest
+import org.springframework.web.reactive.function.server.ServerResponse
+import reactor.core.publisher.Mono
+
+
+data class User(val id: Long, val email: String)
+
+@Component
+class UserHandler {
+    val users = listOf(
+        User(id = 1, email = "easywaldo@test.com"),
+        User(id = 2, email = "easywaldo2@test.com")
+    )
+
+    fun getUser(req: ServerRequest) : Mono<ServerResponse> =
+        users.find {
+            req.pathVariable("id").toLong() == it.id
+        }?.let {
+            ServerResponse.ok().bodyValue(it)
+        }?: ServerResponse.notFound().build()
+
+    fun getAll(req: ServerRequest): Mono<ServerResponse> =
+        ServerResponse.ok().bodyValue(users)
+}
