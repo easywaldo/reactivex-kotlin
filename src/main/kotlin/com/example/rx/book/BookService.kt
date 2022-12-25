07c8b847708e2a4f61250b97883ce913a7bf8863
easywaldo <easywaldoji@gmail.com>
2022-12-25 22:41:55 +0900

    Extenstion function added

--- src/main/kotlin/com/example/rx/book/BookService.kt
+++ src/main/kotlin/com/example/rx/book/BookService.kt
@@ -3,6 +3,8 @@ package com.example.rx.book
 import org.springframework.stereotype.Service
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
+import reactor.kotlin.core.publisher.toFlux
+import reactor.kotlin.core.publisher.toMono
 import java.util.concurrent.atomic.AtomicInteger
 
 data class Book(val id: Int, val name: String, val price: Int)
@@ -11,20 +13,18 @@ data class Book(val id: Int, val name: String, val price: Int)
 class BookService {
 
     private final val nextId = AtomicInteger(0)
-    val books = mutableListOf<Book>(
+    val books: MutableList<Book> = mutableListOf<Book>(
         Book(id =nextId.incrementAndGet(), name = "코틀린 인 액션", price = 10000),
         Book(id =nextId.incrementAndGet(), name = "HTTP 완벽 가이드", price = 20000),
         Book(id =nextId.incrementAndGet(), name = "자바에서 코틀린으로", price = 50000),
     )
 
     fun getAll(): Flux<Book> {
-        return Flux.fromIterable(books)
+        return books.toFlux()
     }
 
     fun get(id: Int): Mono<Book> {
-        return Mono.justOrEmpty(books.find {
-            it.id == id
-        })
+        return books.find { it .id == id }.toMono()
     }
 
     fun add(addBookCommand: Map<String, Any>): Mono<Book> {
